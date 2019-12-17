cdcbc4120a0c80b3980c41b9d53faf8e703d1688
Theo Fenique <theo.fenique@gmail.com>
2019-12-17 22:27:31 +0100

    :sparkles: New event page

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import App from './App'
+import Event from './views/Event'
 import Login from './views/Login'
 import Register from './views/Register'
 
@@ -10,6 +11,7 @@ const Router = () => (
             <Route exact path="/" component={App} />
             <Route exact path="/connexion" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/event" component={Event} />
         </Switch>
     </BrowserRouter>
 )
