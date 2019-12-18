ef0276f5ab23e5be907a6d8e8bb4cbddc2d0e6ed
Anatole Touvron <anatoletouvron@MacBook-Pro-de-Anatole.local>
2019-12-18 15:17:14 +0100

    :sparkles: added eventpage

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import EventCard from './EventCard';
-
+import pinlogo from '../../assets/image/Event/pin.svg'
+import calendarlogo from '../../assets/image/Event/calendar.svg'
+import clocklogo from '../../assets/image/Event/clock.svg'
+import messagelogo from '../../assets/image/Event/message.svg'
+import maplogo from '../../assets/image/Event/map.svg'
 
 let response = require('../../assets/json/MainMenu/response.json');
 
@@ -27,17 +31,72 @@ const StyledMenu = styled.div`
         left: calc(50% - 50px);
         border-radius: 40px;
     }
+
+    .eventDescription {
+    display: flex;
+    margin-bottom:-15px;
+        img{
+        margin-right: 10px;
+        }
+}
+.textDescription{
+    color: #333333;
+    margin-top:5vh;
+    margin-bottom:5vh;
+}
+
+.bottomButton{
+    display:flex;
+    .messageButton{
+        background-color: #3498DB;
+        width:20%;
+        height:50px;
+        display:flex;
+        justify-content:center;
+        align-items: center;
+        border-radius: 15px;
+        img{
+            padding:10px;
+        }
+    }
+    .mapButton{
+        border: solid #3498DB 1px;
+        border-radius: 15px;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        width:75%;
+        height:50px;
+        margin-left:5%;
+        p{
+            color:#3498DB;
+            margin-left:10px;
+        }
+    }
+}
 `
 
+const StyledDivEvent = styled.div`
+    display:flex;
+`
 
 class MainMenu extends Component {
 
     state = {
         currentId: -1,
+        apievent: {}
     }
 
     HandleClick = (id) => {
         console.log("bit")
+    }
+
+    componentDidMount() {
+        if (this.props.SingleEvent) {
+            const resevent = require('../../assets/json/MainMenu/singleevent.json')
+            this.setState({ apievent: resevent[0] })
+            console.log(this.state, resevent)
+        }
     }
 
     render() {
@@ -55,9 +114,27 @@ class MainMenu extends Component {
                     </div>
                     :
                     <div>
-                        <h2>Le nom de l'initiative</h2>
-                        <small className="underTitle">Liste des initiatives à proximité</small>
-
+                        <h2>{this.state.apievent.name}</h2>
+                        <div className="eventDescription">
+                            <img src={pinlogo}></img>
+                            <p className="underTitle">{this.state.apievent.street}</p>
+                        </div>
+                        <div className="eventDescription">
+                            <img src={calendarlogo}></img>
+                            <p className="underTitle">{this.state.apievent.date}</p>
+                        </div>
+                        <div className="eventDescription">
+                            <img src={clocklogo}></img>
+                            <p className="underTitle">{this.state.apievent.hour}</p>
+                        </div>
+                        <p className="textDescription">{this.state.apievent.text}</p>
+                        <div className="bottomButton">
+                            <div className="messageButton"><img src={messagelogo}></img></div>
+                            <div className="mapButton">
+                                <img src={maplogo}></img>
+                                <p>Voir l'ittinéraire</p>
+                            </div>
+                        </div>
                     </div>}
             </StyledMenu>
         );
